f50f66168a1bfbd3933bb9392113ebb36e6044a5
Jose-Currinir <96991994+Jose-Currinir@users.noreply.github.com>
2023-12-22 14:39:07 -0300

    prev deploy v10.1

--- primera/settings.py
+++ primera/settings.py
@@ -119,17 +119,15 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
